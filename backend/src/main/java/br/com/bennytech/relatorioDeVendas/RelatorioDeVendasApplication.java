f19aae82f018765e2a01ade4ce2b6126fc21d7da
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-05 13:55:18 -0300

    Projet Creat
    Primeira parte do projeto com dependências.

---
+++ backend/src/main/java/br/com/bennytech/relatorioDeVendas/RelatorioDeVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bennytech.relatorioDeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelatorioDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelatorioDeVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/bennytech/relatorioDeVendas/RelatorioDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bennytech.relatorioDeVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelatorioDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
