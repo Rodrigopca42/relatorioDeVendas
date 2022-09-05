5daa90fdd68f71c826d8b9135f92b2ee77660fb2
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-05 16:31:20 -0300

    First components
    desenvolvimento dos primeiros icons no reactjs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return (
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   )
 }
 
