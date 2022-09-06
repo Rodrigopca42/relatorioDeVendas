9211c2eeeafb6503217b891ae9dbdcf23a637eef
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-06 10:36:18 -0300

    Other Components
    Toques finais de HTML e CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import NotificationButton from "./components/notificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-    <h1>Olá Mundo!</h1>
-    <NotificationButton/>
-    <NotificationButton/>
-    <NotificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import'./styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/rodrigo-cardoso-b2866b110/"> Rodrigo_Cardoso</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
