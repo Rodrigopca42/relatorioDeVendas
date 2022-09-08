6c4ad9268d96e372b1799f55315def7e2224fc10
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-08 15:40:20 -0300

    Date select
    Configuração de busca de dados por datas

--- backend/src/main/java/br/com/bennytech/relatorioDeVendas/repository/SaleRepository.java
+++ backend/src/main/java/br/com/bennytech/relatorioDeVendas/repository/SaleRepository.java
@@ -1,8 +1,17 @@
 package br.com.bennytech.relatorioDeVendas.repository;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import br.com.bennytech.relatorioDeVendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>  {
 
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+	
 }
