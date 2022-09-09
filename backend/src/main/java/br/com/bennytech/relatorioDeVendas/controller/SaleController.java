1787099b13b153c16abea99c18f9296883e3b116
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-09 10:53:59 -0300

    Twilio SMS
    Configuração e integração do Twilio ao sistema para envio de relatório por sms

--- backend/src/main/java/br/com/bennytech/relatorioDeVendas/controller/SaleController.java
+++ backend/src/main/java/br/com/bennytech/relatorioDeVendas/controller/SaleController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bennytech.relatorioDeVendas.entities.Sale;
 import br.com.bennytech.relatorioDeVendas.services.SaleService;
+import br.com.bennytech.relatorioDeVendas.services.SmsService;
 
 
 @RestController
@@ -19,6 +21,9 @@ public class SaleController {
 	
 	@Autowired
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 
 	@GetMapping
 	public Page<Sale> findSales(
@@ -28,4 +33,9 @@ public class SaleController {
 		return service.findSales(minDate, maxDate, pageable);
 	}
 	
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
 }
