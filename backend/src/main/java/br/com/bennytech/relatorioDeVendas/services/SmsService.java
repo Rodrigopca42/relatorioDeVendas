e7880a39e1b4a416e28a2e5a9f4b201e3bc8ffb5
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-09 11:48:49 -0300

    Autalização

--- backend/src/main/java/br/com/bennytech/relatorioDeVendas/services/SmsService.java
+++ backend/src/main/java/br/com/bennytech/relatorioDeVendas/services/SmsService.java
@@ -35,8 +35,9 @@ public class SmsService {
 	    String date = sale.getDate().getMonthValue() +"/"+ sale.getDate().getYear();
 	
 	    String msg = "O Vendedor " +sale.getSellerName()+ " foi destaque em "+ date 
-	    		+ " com um valor total de R$" + String.format("%.2f", sale.getAmount()); 
-
+	    		+ " com um valor total de R$" + String.format("%.0f", sale.getAmount()); 
+	    
+	  
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
