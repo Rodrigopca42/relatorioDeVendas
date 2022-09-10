95eb29c4680d602e53daee92f75ce20d5c4cc146
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-09-10 17:06:33 -0300

    Toast
    Notificação ao usuário, confirmação do envio do SMS.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import NotificationButton from "./components/notificationButton";
+import { ToastContainer } from 'react-toastify';
+import { Toast } from 'react-toastify/dist/components';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification_icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css';
@@ -10,7 +11,7 @@ type Props ={
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO")
+           toast.info("SMS enviado com sucesso")
         })
 }
 
